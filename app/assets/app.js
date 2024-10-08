4a413f0e6b4f2e97442ae244700e1efb8802e354
granjalahy <gabinranjalahy@gmail.com>
2024-10-08 16:58:58 +0200

    Select gender and redirect to the gender movie's list.

--- app/assets/app.js
+++ app/assets/app.js
@@ -4,3 +4,24 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+const onChecked = (checkbox) =>  {
+    const baseURL = 'http://localhost:8080';
+    if (checkbox.checked) {
+        const genreId = checkbox.value;
+        window.location.href = `${baseURL}/genre/${genreId}/movies`;
+    } else {
+        window.location.href = baseURL;
+    }
+}
+const selectGenre = () => {
+    const genres = document.querySelectorAll('input.cd_gender[type="checkbox"]');
+
+    genres.forEach(genre => {
+        genre.addEventListener('change', (event) => {
+            const target = event.target
+            onChecked(target);
+        });
+    });
+}
+
+selectGenre();
